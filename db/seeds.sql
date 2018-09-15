87164ee6aeb9450c9849582da8949bc64d1247b9
matthewbarrier <barriermatt@gmail.com>
2018-09-15 03:27:39 -0500

    need to add heroku get front page working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Fried Fried Fried burger Fried');
+INSERT INTO burgers (burger_name) VALUES ('Not a Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mcdowell Burger');
