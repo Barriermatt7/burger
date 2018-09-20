ddd14127b3a3a3c993c8c288b547851eac0148ba
matthewbarrier <barriermatt@gmail.com>
2018-09-20 05:24:23 -0500

    latest update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
 INSERT INTO burgers (burger_name) VALUES ('Fried Fried Fried burger Fried');
 INSERT INTO burgers (burger_name) VALUES ('Not a Burger');
 INSERT INTO burgers (burger_name) VALUES ('Mcdowell Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Giant Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Tex Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('little Burger', false);
